1ae3cb27ea2d33f49182e9bb2d4a45713a873a93
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-03-03 13:54:36 +0000

    Lagt inn fil for preprossesering av data i norartritt-pakke
    SVN-revisjon: 6488

---
+++ felles/pakker/norartritt/R/data-prep-norartritt.R
@@ -0,0 +1,11 @@
+
+# Preprossesering av data for NorArtritt
+
+# Lage en funksjon for å forberede alle datasett i NorArtritt for analyse.
+# Hovedfokus er for bruk i årsrapport, men jeg antar objektene vil være
+# å foretrekke i øvrige analyser også.
+
+# TODO
+# Liste opp alt som skal forbedres i alle de ulike objektene
+# Generelle ting som skal fikses i flere at objektene
+#
