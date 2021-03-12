81a3008ce9383175c8a3fd7d36832a03f7fd1a69
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-03-12 13:42:07 +0000

    Lagt inn tester for velg_tidligste_inklusjonsdato
    SVN-revisjon: 6563

--- felles/pakker/norartritt/tests/testthat/test-hjelpefunksjoner-norartritt.R
+++ felles/pakker/norartritt/tests/testthat/test-hjelpefunksjoner-norartritt.R
@@ -14,3 +14,52 @@ context("legg_til_diagnosegrupper")
 
 context("velg_tidligste_inklusjondato")
 # Sjekk at NA returnerees hvis det ikke finnes dato på noen av skjema
+dato = c("NA", "NA", "2020-11-23", "2020-08-27")
+d = tibble::tibble(
+  PasientGUID = c("A", "A", "B", "B"),
+  InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
+) # Sjekke om det skal være HMS her
+
+d_ut_forventet = tibble::tibble(
+  PasientGUID = c("A", "A", "B", "B"),
+  InklusjonDato = as.Date(c(NA, NA, "2020-08-27", "2020-08-27"))
+)
+
+expect_identical(
+  velg_tidligste_inklusjondato(pas_id = PasientGUID, d),
+  d_ut_forventet
+)
+
+# Sjekk at tidligste tilgjengelige dato velges hvis det også finnes NA
+dato = c("NA", "2019-11-12", "2020-11-23", "2020-08-27")
+d = tibble::tibble(
+  PasientGUID = c("A", "A", "B", "B"),
+  InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
+) # Sjekke om det skal være HMS her
+
+d_ut_forventet = tibble::tibble(
+  PasientGUID = c("A", "A", "B", "B"),
+  InklusjonDato = as.Date(c("2019-11-12", "2019-11-12", "2020-08-27", "2020-08-27"))
+)
+
+expect_identical(
+  velg_tidligste_inklusjondato(pas_id = PasientGUID, d),
+  d_ut_forventet
+)
+
+# Sjekk at funksjonen aksepterer annen pas_id
+dato = c("NA", "2019-11-12", "2020-11-23", "2020-08-27")
+d = tibble::tibble(
+  fnr = c(1, 1, 2, 2),
+  InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
+)
+
+d_ut_forventet = tibble::tibble(
+  fnr = c(1, 1, 2, 2),
+  InklusjonDato = as.Date(c("2019-11-12", "2019-11-12", "2020-08-27", "2020-08-27"))
+)
+
+expect_identical(
+  velg_tidligste_inklusjondato(pas_id = fnr, d),
+  d_ut_forventet
+)
