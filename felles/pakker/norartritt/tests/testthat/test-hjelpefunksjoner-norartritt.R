d04487770fb63c0568a36bcbdfd8f5fdaf2d59f0
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-03-22 09:05:57 +0000

    Lagt inn test_that-kall for alle tester
    SVN-revisjon: 6643

--- felles/pakker/norartritt/tests/testthat/test-hjelpefunksjoner-norartritt.R
+++ felles/pakker/norartritt/tests/testthat/test-hjelpefunksjoner-norartritt.R
@@ -1,81 +1,57 @@
 # tester hjelpefunksjoner-norartritt.R
 
 context("legg_til_medisinnavn")
-# Gir advarsel hvis det finnes en LegemiddelType som ikke har navn i kodebok
-d = tibble::tibble(
-  LegemiddelType = c(1, 2, 3, 765),
-  Legemiddel = c(NA, NA, NA, NA)
-)
 
-d_ut_forventet = tibble::tibble(
-  LegemiddelType = c(1, 2, 3),
-  Legemiddel = c(NA, NA, NA),
-  legemiddel_navn_kode = c(1L, 2L, 3L),
-  legemiddel_navn = c(
-    "Cimzia (certolizumab pegol)",
-    "Enbrel (etanercept)",
-    "Humira (adalimumab)"
-  ),
-  biokat = c(1L, 1L, 1L),
-  dmard = c(1L, 1L, 1L),
-  csdmard = c(0L, 0L, 0L),
-  tsdmard = c(0L, 0L, 0L),
-  bio_og_tsdmard = c(1L, 1L, 1L),
-  legemiddel_gruppert = c(1L, 2L, 3L),
-  legemiddel_gruppert_navn = c(
-    "Cimzia (certolizumab pegol)",
-    "Enbrel (etanercept)",
-    "Humira (adalimumab)"
-  ),
-  Virkestoff = c(
-    "certolizumab pegol",
-    "etanercept",
-    "adalimumab"
+test_that("Advarsel hvis data inneholder LegemiddelType som ikke har navn i kodebok", {
+  d = tibble::tibble(
+    LegemiddelType = c(1, 2, 3, 765),
+    Legemiddel = c(NA, NA, NA, NA)
   )
-)
 
-expect_error(
-  legg_til_medisinnavn(d),
-  "LegemiddelType 765 er ikke definert i medisinkodeboken"
-)
-
-# Funksjonen leser inn medisinnavn som forventet
-d = tibble::tibble(
-  LegemiddelType = c(1, 2, 3),
-  Legemiddel = c(NA, NA, NA)
-)
-
-d_ut_forventet = tibble::tibble(
-  LegemiddelType = c(1, 2, 3),
-  Legemiddel = c(NA, NA, NA),
-  legemiddel_navn_kode = c(1L, 2L, 3L),
-  legemiddel_navn = c(
-    "Cimzia (certolizumab pegol)",
-    "Enbrel (etanercept)",
-    "Humira (adalimumab)"
-  ),
-  biokat = c(1L, 1L, 1L),
-  dmard = c(1L, 1L, 1L),
-  csdmard = c(0L, 0L, 0L),
-  tsdmard = c(0L, 0L, 0L),
-  bio_og_tsdmard = c(1L, 1L, 1L),
-  legemiddel_gruppert = c(1L, 2L, 3L),
-  legemiddel_gruppert_navn = c(
-    "Cimzia (certolizumab pegol)",
-    "Enbrel (etanercept)",
-    "Humira (adalimumab)"
-  ),
-  Virkestoff = c(
-    "certolizumab pegol",
-    "etanercept",
-    "adalimumab"
+  expect_error(
+    legg_til_medisinnavn(d),
+    "LegemiddelType 765 er ikke definert i medisinkodeboken"
   )
-)
+})
 
-expect_identical(
-  legg_til_medisinnavn(d),
-  d_ut_forventet
-)
+test_that("Leser inn data fra medisinkodebok som forventet", {
+  d = tibble::tibble(
+    LegemiddelType = c(1, 2, 3),
+    Legemiddel = c(NA, NA, NA)
+  )
+
+  d_ut_forventet = tibble::tibble(
+    LegemiddelType = c(1, 2, 3),
+    Legemiddel = c(NA, NA, NA),
+    legemiddel_navn_kode = c(1L, 2L, 3L),
+    legemiddel_navn = c(
+      "Cimzia (certolizumab pegol)",
+      "Enbrel (etanercept)",
+      "Humira (adalimumab)"
+    ),
+    biokat = c(1L, 1L, 1L),
+    dmard = c(1L, 1L, 1L),
+    csdmard = c(0L, 0L, 0L),
+    tsdmard = c(0L, 0L, 0L),
+    bio_og_tsdmard = c(1L, 1L, 1L),
+    legemiddel_gruppert = c(1L, 2L, 3L),
+    legemiddel_gruppert_navn = c(
+      "Cimzia (certolizumab pegol)",
+      "Enbrel (etanercept)",
+      "Humira (adalimumab)"
+    ),
+    Virkestoff = c(
+      "certolizumab pegol",
+      "etanercept",
+      "adalimumab"
+    )
+  )
+
+  expect_identical(
+    legg_til_medisinnavn(d),
+    d_ut_forventet
+  )
+})
 
 context("legg_til_sykehusnavn")
 # Gir advarsel om det finnes UnitID som ikke har navn i kodebok
@@ -85,53 +61,58 @@ context("legg_til_diagnosegrupper")
 # Gir advarsel om noen mangler Kode
 
 context("velg_tidligste_inklusjondato")
-# Sjekk at NA returnerees hvis det ikke finnes dato på noen av skjema
-dato = c("NA", "NA", "2020-11-23", "2020-08-27")
-d = tibble::tibble(
-  PasientGUID = c("A", "A", "B", "B"),
-  InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
-) # Sjekke om det skal være HMS her
 
-d_ut_forventet = tibble::tibble(
-  PasientGUID = c("A", "A", "B", "B"),
-  InklusjonDato = as.Date(c(NA, NA, "2020-08-27", "2020-08-27"))
-)
+test_that("NA returneres hvis ingen inklusjonsdato finnes", {
+  dato = c("NA", "NA", "2020-11-23", "2020-08-27")
+  d = tibble::tibble(
+    PasientGUID = c("A", "A", "B", "B"),
+    InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
+  ) # Sjekke om det skal være HMS her
 
-expect_identical(
-  velg_tidligste_inklusjondato(pas_id = PasientGUID, d),
-  d_ut_forventet
-)
+  d_ut_forventet = tibble::tibble(
+    PasientGUID = c("A", "A", "B", "B"),
+    InklusjonDato = as.Date(c(NA, NA, "2020-08-27", "2020-08-27"))
+  )
 
-# Sjekk at tidligste tilgjengelige dato velges hvis det også finnes NA
-dato = c("NA", "2019-11-12", "2020-11-23", "2020-08-27")
-d = tibble::tibble(
-  PasientGUID = c("A", "A", "B", "B"),
-  InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
-) # Sjekke om det skal være HMS her
+  expect_identical(
+    velg_tidligste_inklusjondato(pas_id = PasientGUID, d),
+    d_ut_forventet
+  )
+})
 
-d_ut_forventet = tibble::tibble(
-  PasientGUID = c("A", "A", "B", "B"),
-  InklusjonDato = as.Date(c("2019-11-12", "2019-11-12", "2020-08-27", "2020-08-27"))
-)
+test_that("Tidligste dato returneres hvis det også finnes NA", {
+  dato = c("NA", "2019-11-12", "2020-11-23", "2020-08-27")
+  d = tibble::tibble(
+    PasientGUID = c("A", "A", "B", "B"),
+    InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
+  ) # Sjekke om det skal være HMS her
 
-expect_identical(
-  velg_tidligste_inklusjondato(pas_id = PasientGUID, d),
-  d_ut_forventet
-)
+  d_ut_forventet = tibble::tibble(
+    PasientGUID = c("A", "A", "B", "B"),
+    InklusjonDato = as.Date(c("2019-11-12", "2019-11-12", "2020-08-27", "2020-08-27"))
+  )
+
+  expect_identical(
+    velg_tidligste_inklusjondato(pas_id = PasientGUID, d),
+    d_ut_forventet
+  )
+})
 
 # Sjekk at funksjonen aksepterer annen pas_id
-dato = c("NA", "2019-11-12", "2020-11-23", "2020-08-27")
-d = tibble::tibble(
-  fnr = c(1, 1, 2, 2),
-  InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
-)
+test_that("Fungerer med alternativ pasientidentifikator", {
+  dato = c("NA", "2019-11-12", "2020-11-23", "2020-08-27")
+  d = tibble::tibble(
+    fnr = c(1, 1, 2, 2),
+    InklusjonDato = (as.POSIXct(dato, tz = "UTC", format = "%Y-%m-%d"))
+  )
 
-d_ut_forventet = tibble::tibble(
-  fnr = c(1, 1, 2, 2),
-  InklusjonDato = as.Date(c("2019-11-12", "2019-11-12", "2020-08-27", "2020-08-27"))
-)
+  d_ut_forventet = tibble::tibble(
+    fnr = c(1, 1, 2, 2),
+    InklusjonDato = as.Date(c("2019-11-12", "2019-11-12", "2020-08-27", "2020-08-27"))
+  )
 
-expect_identical(
-  velg_tidligste_inklusjondato(pas_id = fnr, d),
-  d_ut_forventet
-)
+  expect_identical(
+    velg_tidligste_inklusjondato(pas_id = fnr, d),
+    d_ut_forventet
+  )
+})
