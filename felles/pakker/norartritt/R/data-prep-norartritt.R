485a43121b6ae6a5a1478741c4e663035139fe88
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-04-26 09:49:55 +0000

    Lagt inn funksjon lag_filtrerte_objekter og utkast til vask_data_norartritt
    SVN-revisjon: 6803

--- felles/pakker/norartritt/R/data-prep-norartritt.R
+++ felles/pakker/norartritt/R/data-prep-norartritt.R
@@ -11,88 +11,127 @@
 # Funksjonen skal implementeres i les_data_norartritt() slik at en får ut
 # både fullstendige datasett og ferdig prosseserte datasett.
 
-# Inklusjon
-# (Rett inklusjonsdato) Trekke ut tidligste inklusjonsdato fra inklusjon/oppfølgingsskjema
-# (Rett_skjematype) Konvertere første oppfølgingsskjema til inklusjonsskjema.
-# Oppfølging
-# Medisin
-# Diagnose
-
 # Fjerne skjema - eksempel - fjerne diagnose hvis inklusjon mangler.
 # Filtrere bort pasienter som ikke har aktuelle diagnoser.
 # Har de kun uaktuelle diagnoser skal de ikke være med i registeret i det hele tatt
 
-
-# Vurdere om det er nødvendig med mini diagnosekodebøker.
-# Hent heller inn diagnosegrupper i dataprep
-
-
-# # Lage datovariabler:
-#   # Oppfølging:
-#   - aar_ktrl = year(Formdate)
-#   - dato_ktrl = as_date(FormDate)
-#   # Diagnose:
-#   - aar = year(Dato)
-#   - dato_diag = as_date(FormDate)
-#   - diag_stilt_aar = year(dato_diag)
-#   - dager_diag_til_datadump = ymd(dato_datadump)-ymd(dato_diag)
-#   # Medisin:
-#   - startaar = year(StartDato)
-#   - StartDato = as_date(StartDato)
-#   - sluttaar = year(SluttDato)
-#   - SluttDato = as_date(SluttDato)
-#   # Inklusjon:
-#   - aar_ktrl = year(InklusjonDato)
-#   - dato_ktrl = as_date(InklusjonDato)
-#
-# # Lage nyttige objekter?
-#   #d_med_vasket
-#   - Fjerne missing StartDato
-#   - Fjerne duplikatregistrering av samme legemiddel på samme StartDato
-#     - Prioritere de som har sluttdato av duplikater.
-#   # d_diag_pers
-#   - kun siste diagnose for hver pasient
-#   # d_diag_med
-#   - Siste diagnose, hele medisinhistorikken
-#   - Legge inn kode 99 for pasienter som ikke går på noen medisin.
-#   # d_diag_aktmed
-#   - siste diagnose og aktive medisinforløp i rapporteringsår.
-#
+# FIXME - Flytte hjelpefunksjoner til dette skriptet.
 
 # Kjernefunksjon som kaller på alle underfunksjoner
 vask_data_norartritt = function(d_inkl, d_oppf, d_diag, d_med) {
-
+  d_inkl_oppf = d_inkl %>%
+    bind_rows(d_oppf)
   # Rett inklusjonsdato
-  # velg_tidligste_inklusjonsdato() #FIXME - Flytte funksjonen hit.
+  d_inkl_oppf = velg_tidligste_inklusjondato(d_inkl_oppf = d_inkl_oppf)
   # Rett skjematype
-  # velg_rett_skjematype()
+  d_inkl_oppf = konverter_skjematype(inkl = d_inkl, oppf = d_oppf)
+  # Pakk ut inklusjons og oppfølgingsskjema.
+  d_inkl = d_inkl_oppf %>%
+    filter(FormTypeId == 1)
+  d_oppf = d_inkl_oppf %>%
+    filter(FormTypeId == 2)
   # Filtrer bort ugyldige skjema
+  fjern_ugyldige_skjema(inkl = d_inkl, oppf = d_oppf, diag = d_diag, med = d_med) # Returnerer filtrerte objekter
+
   # Legg til datovariabler
+  skjema = legg_til_datovariabler(
+    d_inkl = inkl_filtrert, d_oppf = oppf_filtrert,
+    d_diag = diag_filtrert, d_med = med_filtrert
+  )
+
+  d_inkl = skjema[[1]]
+  d_oppf = skjema[[2]]
+  d_diag = skjema[[3]]
+  d_med = skjema[[4]]
+
   # strukturer variabler som mangler kodebok
+
   # Lag utvalgte ferdigfiltrerte objekter
+
   # Returner data
 }
 
-velg_rett_skjematype = function(d_inkl, d_oppf) {
 
-  # Konvertere tidligste oppfølgingsskjema til inklusjonsskjema for de
-  # pasientene som mangler inklusjon.
-  # # Legge inn regel for valg hvis det er flere skjema på samme dato
-  # Returnere d_inkl og d_oppf med oppdaterte skjematyper.
-}
-
-filtrer_bort_ugyldige_skjema = function(d_inkl, d_oppf, d_diag, d_med) {
-
-}
-
-legg_til_ekstra_datovariabler = function(d_inkl, d_oppf, d_diag, d_med) {
-
-}
-
+# FIXME - Dokumentasjon
 lag_filtrerte_objekter = function(d_inkl, d_oppf, d_diag, d_med) {
 
+  # medisinforløp for hver pasient hvor duplikater er fjernet
+  d_med_vasket = d_med %>%
+    filter(!is.na(StartDato))
+
+  skjemaid_duplikat = d_med_vasket %>%
+    add_count(PasientGUID, StartDato, LegemiddelType) %>%
+    filter(n > 1) %>%
+    group_by(PasientGUID) %>%
+    arrange(SluttDato) %>%
+    distinct(PasientGUID, .keep_all = TRUE) %>%
+    select(-n)
+
+  d_med_vasket = setdiff(d_med_vasket, skjemaid_duplikat)
+
+  # Siste diagnose for hver pasient
+  d_diag_pers = d_diag %>%
+    arrange(desc(dato_diag)) %>%
+    distinct(PasientGUID, .keep_all = TRUE)
+
+  # d_diag_med: Siste diagnose og hele medisinhistorikken, inkludert Ingen medisin
+  # for de som ikke har medisinsk behandling
+  d_diag_med = d_diag_pers %>%
+    select(
+      PasientGUID, UnitId, FormDate, PatientAge, PatientGender, Kode, Navn, Dato, DiagnoseDato,
+      aar, dato_diag, diag_stilt_aar
+    ) %>%
+    left_join(d_med_vasket %>%
+      select(
+        PasientGUID, Enhet, Mengde, LegemiddelType, Legemiddel, Intervall,
+        EndringArsak, StartDato, SluttDato, EndringsDato,
+        startaar, sluttaar
+      ), by = "PasientGUID") %>%
+    legg_til_medisinnavn() %>% # FIXME - Flytte denne til tidligere objekt.
+    legg_til_sykehusnavn() %>% # FIXME - Flytte til tidligere objekt
+    mutate(
+      legemiddel_navn = replace(
+        as.character(legemiddel_navn),
+        is.na(legemiddel_navn), "Ingen medisin"
+      ),
+      legemiddel_navn_kode = replace(
+        as.integer(legemiddel_navn_kode),
+        is.na(legemiddel_navn_kode), 99L
+      ),
+      biokat = replace(biokat, is.na(biokat), 99L),
+      csdmard = ifelse(is.na(LegemiddelType), yes = 0L,
+        no = csdmard
+      ),
+      dmard = ifelse(is.na(LegemiddelType), yes = 0L,
+        no = dmard
+      ),
+      tsdmard = ifelse(is.na(LegemiddelType), yes = 0L,
+        no = tsdmard
+      ),
+      bio_og_tsdmard = ifelse(is.na(LegemiddelType), yes = 0L,
+        no = bio_og_tsdmard
+      ),
+      legemiddel_gruppert = replace(
+        as.integer(legemiddel_gruppert),
+        is.na(legemiddel_gruppert), 99L
+      ),
+      legemiddel_gruppert_navn = replace(
+        as.character(legemiddel_gruppert_navn),
+        is.na(legemiddel_gruppert_navn), "Ingen medisin"
+      ),
+      Virkestoff = replace(
+        as.character(Virkestoff),
+        is.na(Virkestoff), "Ingen medisin"
+      ),
+      diagnose_aar = lubridate::year(dato_diag)
+    )
+
+  assign("d_med_vasket", d_med_vasket, envir = .GlobalEnv)
+  assign("d_diag_pers", d_diag_pers, envir = .GlobalEnv)
+  assign("d_diag_med", d_diag_med, envir = .GlobalEnv)
 }
 
-strukturer_variabler_uten_kodebok = function(d_inkl, d_oppf, d_diag, d_med) {
 
+
+strukturer_variabler_uten_kodebok = function(d_inkl, d_oppf, d_diag, d_med) {
 }
