35d936c561f4820b8db080d291b07086562b351c
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2025-07-14 10:33:34 +0200

    Bruk sentralisert importfil

---
+++ nasjonale-register/roas/r-pakkar/roas/R/roas-package.R
@@ -0,0 +1,33 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom DBI dbConnect
+#' @importFrom DBI dbGetQuery
+#' @importFrom DBI dbIsValid
+#' @importFrom dplyr across
+#' @importFrom dplyr add_row
+#' @importFrom dplyr arrange
+#' @importFrom dplyr case_when
+#' @importFrom dplyr count
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr full_join
+#' @importFrom dplyr group_by
+#' @importFrom dplyr group_indices
+#' @importFrom dplyr if_else
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr rowwise
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+#' @importFrom glue glue
+#' @importFrom lubridate month
+#' @importFrom lubridate year
+#' @importFrom tibble as_tibble
+#' @importFrom tibble tibble
+#' @importFrom tidyr pivot_wider
+## usethis namespace: end
+NULL
